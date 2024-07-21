7d078e69fa151c7ac738381238f1a5d8558e6b36
macieksm19 <maciek.smieja05@icloud.com>
2024-07-21 09:30:47 +0200

    Fixed validatePesel() and convertPeselToDateOfBirth

--- src/User.java
+++ src/User.java
@@ -15,6 +15,10 @@ public class User {
         this.userLoginData = userLoginData;
         user.put(userLoginData.getLogin(), userLoginData.getPassword());
     }
+
+    public UserLoginData getUserLoginData() {
+        return userLoginData;
+    }
 }
 
 class UserPersonalData {
@@ -31,17 +35,31 @@ class UserPersonalData {
     }
 
     boolean validatePesel() {
-        if (pesel.length() == 11 && Character.getNumericValue(pesel.charAt(2)) >= 1 && Character.getNumericValue(pesel.charAt(2)) <= 3 && Character.getNumericValue(pesel.charAt(3)) <= 2) {
+        if (pesel.length() == 11 &&
+                Character.getNumericValue(pesel.charAt(2)) <= 3 &&
+                (((Character.getNumericValue(pesel.charAt(2)) == 0 || Character.getNumericValue(pesel.charAt(2)) == 1) &&
+                Character.getNumericValue(pesel.charAt(3)) <= 2) ||
+                (Character.getNumericValue(pesel.charAt(2)) <= 0 || Character.getNumericValue(pesel.charAt(3)) == 2)) &&
+                Character.getNumericValue(pesel.charAt(4)) <= 3 &&
+                ((Character.getNumericValue(pesel.charAt(4)) == 3) && (Character.getNumericValue(pesel.charAt(5)) == 0) || Character.getNumericValue(pesel.charAt(5)) == 1) || ((Character.getNumericValue(pesel.charAt(4)) <= 2) && Character.getNumericValue(pesel.charAt(5)) >= 0)) {
             System.out.println("PESEL jest poprawny");
             return true;
         } else if (pesel.length() != 11){
             System.out.println("Dlugosc numeru PESEL jest niepoprawna. Sprobuj jeszcze raz.");
             return false;
-        } else if (!(Character.getNumericValue(pesel.charAt(2)) > 0 && Character.getNumericValue(pesel.charAt(2)) < 4)) {
+        } else if (!(Character.getNumericValue(pesel.charAt(2)) <= 3)) {
             System.out.println("3 cyfra numeru PESEL jest niepoprawna. Sprobuj jeszcze raz.");
             return false;
-        } else if (!((int) Character.getNumericValue(pesel.charAt(3)) <= 2)){
+        } else if (!(((Character.getNumericValue(pesel.charAt(2)) == 0 || Character.getNumericValue(pesel.charAt(2)) == 1) &&
+                Character.getNumericValue(pesel.charAt(3)) <= 2) ||
+                (Character.getNumericValue(pesel.charAt(2)) <= 0 || Character.getNumericValue(pesel.charAt(3)) == 2))){
             System.out.println("4 cyfra numeru PESEL jest niepoprawna. Sprobuj jeszcze raz.");
+            return false;
+        } else if (!(Character.getNumericValue(pesel.charAt(4)) <= 3)) {
+            System.out.println("5 cyfra numeru PESEL jest niepoprawna. Sprobuj jeszcze raz.");
+            return false;
+        } else if (!(((Character.getNumericValue(pesel.charAt(4)) == 3) && (Character.getNumericValue(pesel.charAt(5)) == 0) || Character.getNumericValue(pesel.charAt(5)) == 1) || ((Character.getNumericValue(pesel.charAt(4)) <= 2) && Character.getNumericValue(pesel.charAt(5)) >= 0))) {
+            System.out.println("6 cyfra numeru PESEL jest niepoprawna. Sprobuj jeszcze raz.");
             return false;
         } else {
             System.out.println("Napotkano problem.");
@@ -52,29 +70,23 @@ class UserPersonalData {
     void convertPeselToDateOfBirth() {
         String year = null;
         String month = null;
-        String day = null;
 
         if (Character.getNumericValue(pesel.charAt(2)) == 1) {
             year = "19" + pesel.charAt(0) + pesel.charAt(1);
-            month = String.valueOf(pesel.charAt(2) + pesel.charAt(3));
+            month = String.valueOf(pesel.charAt(2)) + pesel.charAt(3);
         } else if(Character.getNumericValue(pesel.charAt(2)) == 2 || Character.getNumericValue(pesel.charAt(2)) == 3) {
             year = "20" + Character.getNumericValue(pesel.charAt(0)) + Character.getNumericValue(pesel.charAt(1));
             month = String.valueOf((Character.getNumericValue(pesel.charAt(2)) + Character.getNumericValue(pesel.charAt(3)) - 20));
-
             if (Integer.valueOf(month) < 10)
-                month = "0" + Character.getNumericValue(pesel.charAt(3));
+                month = "0" + pesel.charAt(3);
         }
 
-        day = String.valueOf(pesel.charAt(4)) + (pesel.charAt(5));
+        String day = String.valueOf(pesel.charAt(4)) + pesel.charAt(5);
         if (day != null && month != null && year != null) {
             dateOfBirth = LocalDate.parse(year + "-" + month + "-" + day);
             System.out.println("Twoja data urodzin: " + dateOfBirth);
         } else
             System.out.println("Wystapil blad w konwersji");
-    }
-
-    public String getFirstName() {
-        return firstName;
     }
 
     public void setFirstName(String firstName) {
