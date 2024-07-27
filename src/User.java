4398007aaa3188cb16d68c24f5ac3c0a63dece52
macieksm19 <maciek.smieja05@icloud.com>
2024-07-27 14:00:20 +0200

    Changed class User to record

--- src/User.java
+++ src/User.java
@@ -1,10 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class User {
-    private final UserPersonalData userPersonalData;
-    private final UserContact userContactData;
-    private final Credentials userLoginData;
+public record User(UserPersonalData userPersonalData, UserContact userContactData, Credentials userLoginData) {
     static Map<String, String> userLoginAndPassword = new HashMap<>();
 
     public User(UserPersonalData userPersonalData, UserContact userContactData, Credentials userLoginData) {
@@ -12,18 +9,6 @@ public class User {
         this.userContactData = userContactData;
         this.userLoginData = userLoginData;
         userLoginAndPassword.put(userLoginData.getLogin(), userLoginData.getPassword());
-    }
-
-    public Credentials getUserLoginData() {
-        return userLoginData;
-    }
-
-    public UserPersonalData getUserPersonalData() {
-        return userPersonalData;
-    }
-
-    public UserContact getUserContactData() {
-        return userContactData;
     }
 }
 
