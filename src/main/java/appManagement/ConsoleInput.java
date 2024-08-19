34f19c75696487a666bafa91958dc17525c30c7e
macieksm19 <maciek.smieja05@icloud.com>
2024-08-19 09:30:37 +0200

    Added ConsoleInput class

--- src/main/java/appManagement/ConsoleInput.java
+++ src/main/java/appManagement/ConsoleInput.java
@@ -1,2 +1,19 @@
-package appManagement;public class ConsoleInput {
+package appManagement;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class ConsoleInput {
+    private final Scanner scanner;
+    public ConsoleInput(InputStream inputStream) {
+        this.scanner = new Scanner(inputStream);
+    }
+
+    public String getStringInput() {
+        return scanner.nextLine();
+    }
+
+    public int getIntInput() {
+        return scanner.nextInt();
+    }
 }
