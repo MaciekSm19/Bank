c4fe50fa8e37db1b65b1843de3b888754e0ef6c1
macieksm19 <maciek.smieja05@icloud.com>
2024-08-19 15:17:17 +0200

    Created helper methods for tests

--- src/test/java/appManagement/ConsoleInputTest.java
+++ src/test/java/appManagement/ConsoleInputTest.java
@@ -9,30 +9,39 @@ import java.io.InputStream;
 import java.math.BigDecimal;
 
 class ConsoleInputTest {
-    @Test
-    public void testStringInput() {
-        String value = "name";
-        InputStream inputStream = new ByteArrayInputStream(value.getBytes());
+    private void stringValid(String expected, String actual) {
+        InputStream inputStream = new ByteArrayInputStream(actual.getBytes());
         ConsoleInput consoleInput = new ConsoleInput(inputStream);
-        assertEquals("name", consoleInput.getStringInput());
+        assertEquals(expected, consoleInput.getStringInput());
     }
-
-    @Test
-    public void testIntInput() {
-        String value = "123";
-        InputStream inputStream = new ByteArrayInputStream(value.getBytes());
+    private void intValid(int expected, String actual) {
+        InputStream inputStream = new ByteArrayInputStream(actual.getBytes());
         ConsoleInput consoleInput = new ConsoleInput(inputStream);
         int result = consoleInput.getIntInput();
-        assertEquals(123, result);
+        assertEquals(expected, result);
     }
-
+    private void bigDecimalValid(BigDecimal expected, String actual) {
+        InputStream inputStream = new ByteArrayInputStream(actual.getBytes());
+        ConsoleInput consoleInput = new ConsoleInput(inputStream);
+        BigDecimal result = consoleInput.getBigDecimalInput();
+        assertEquals(expected, result);
+    }
+    @Test
+    public void testStringInput() {
+        stringValid("Name", "Name");
+        stringValid("Jan", "Jan");
+        stringValid("Kowalski", "Kowalski");
+    }
+    @Test
+    public void testIntInput() {
+        intValid(1, "1");
+        intValid(100, "100");
+        intValid(21, "21");
+    }
     @Test
     public void testBigDecimalInput() {
-        String value = "1000.5643";
-        InputStream inputStream = new ByteArrayInputStream(value.getBytes());
-        ConsoleInput consoleInput = new ConsoleInput(inputStream);
-
-        BigDecimal result = consoleInput.getBigDecimalInput();
-        assertEquals(new BigDecimal("1000.5643"), result);
+        bigDecimalValid(BigDecimal.valueOf(11.22), "11.22");
+        bigDecimalValid(BigDecimal.valueOf(1100012.22000021), "1100012.22000021");
+        bigDecimalValid(BigDecimal.valueOf(213489.98874), "213489.98874");
     }
 }
